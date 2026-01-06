d59def2299aa5c58740ecb449556bbd8e9d64a95
Baolink211 <nguyenquocbaolinh21199@gmail.com>
2026-01-06 22:57:15 +0700

    init commit

---
+++ src/test/java/test/inventory/AddToCart.java
@@ -0,0 +1,38 @@
+package test.inventory;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import utils.WebElementUtils;
+
+public class InventoryManagement {
+
+    WebDriver driver;
+    WebElementUtils elementUtils;
+
+    @Before
+    public void setup() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+
+        //  KHỞI TẠO DRIVER
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        elementUtils = new WebElementUtils(driver);
+
+        //  LOGIN
+        elementUtils.sendKeys("id", "user-name", "standard_user");
+        elementUtils.sendKeys("id", "password", "secret_sauce");
+        elementUtils.click("id", "login-button");
+    }
+    @Test
+    public void InventoryManagement()
+    {
+
+    }
+}
