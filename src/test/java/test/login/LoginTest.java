5ed10e7602d1bec801db59b23f939ddb6339847d
Baolink211 <nguyenquocbaolinh21199@gmail.com>
2026-01-06 21:12:17 +0700

    innit commit

---
+++ src/test/java/test/login/LoginTest.java
@@ -0,0 +1,110 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import utils.ExcelUtils;
+import utils.WebElementUtils;
+
+import java.util.List;
+import java.util.Map;
+
+public class LoginTest {
+
+    WebDriver driver;
+    WebElementUtils elementUtils;
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+
+        elementUtils = new WebElementUtils(driver);
+    }
+
+    @Test
+    public void happyCaseLogin() {
+
+        // 1️⃣ Đọc dữ liệu từ Excel
+        List<Map<String, String>> testData =
+                ExcelUtils.readExcel(
+                        "src/test/resources/testdata.xlsx",
+                        "HappyCase"
+                );
+
+        // 2️⃣ Chạy từng dòng dữ liệu
+        for (Map<String, String> data : testData) {
+
+            String username = data.get("username");
+            String password = data.get("password");
+
+            // 3️⃣ Thao tác UI qua WebElementUtils
+            elementUtils.sendKeys("id", "user-name", username);
+            elementUtils.sendKeys("id", "password", password);
+            elementUtils.click("id", "login-button");
+
+            // reset lại form cho case tiếp theo
+            driver.navigate().refresh();
+        }
+    }
+    @Test
+    public void UpperCaseLogin() {
+
+        // 1️⃣ Đọc dữ liệu từ Excel
+        List<Map<String, String>> testData =
+                ExcelUtils.readExcel(
+                        "src/test/resources/testdata.xlsx",
+                        "UpperCase"
+                );
+
+        // 2️⃣ Chạy từng dòng dữ liệu
+        for (Map<String, String> data : testData) {
+
+            String username = data.get("username");
+            String password = data.get("password");
+
+            // 3️⃣ Thao tác UI qua WebElementUtils
+            elementUtils.sendKeys("id", "user-name", username);
+            elementUtils.sendKeys("id", "password", password);
+            elementUtils.click("id", "login-button");
+
+            // reset lại form cho case tiếp theo
+            driver.navigate().refresh();
+        }
+    }
+    @Test
+    public void UnhappyCaseLogin() {
+
+        // 1️⃣ Đọc dữ liệu từ Excel
+        List<Map<String, String>> testData =
+                ExcelUtils.readExcel(
+                        "src/test/resources/testdata.xlsx",
+                        "UnhappyCase"
+                );
+
+        // 2️⃣ Chạy từng dòng dữ liệu
+        for (Map<String, String> data : testData) {
+
+            String username = data.get("username");
+            String password = data.get("password");
+
+            // 3️⃣ Thao tác UI qua WebElementUtils
+            elementUtils.sendKeys("id", "user-name", username);
+            elementUtils.sendKeys("id", "password", password);
+            elementUtils.click("id", "login-button");
+
+            // reset lại form cho case tiếp theo
+            driver.navigate().refresh();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
